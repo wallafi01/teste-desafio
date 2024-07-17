14e84c0f658772802c610c8d4cabf67e7058d573
wallafi <wallafisantos@gmail.com>
2024-07-17 02:34:45 -0300

    add

--- src/main.tf
+++ src/main.tf
@@ -26,3 +26,25 @@ module "codedeploy" {
   instance_name = var.instance_name
 }
 
+output "ec2_public_ip" {
+  value = module.ec2-resources.ec2_public_ip
+}
+
+output "ec2_instance_id" {
+  value = module.ec2-resources.ec2_instance_id
+}
+
+
+output "codedeploy_app_name" {
+  description = "The name of the CodeDeploy application"
+  value       = module.codedeploy.codedeploy_app_name
+}
+
+output "codedeploy_deployment_group_name" {
+  description = "The name of the CodeDeploy deployment group"
+  value       = module.codedeploy.codedeploy_deployment_group_name
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.code_deploy
+}
