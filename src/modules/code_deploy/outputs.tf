07edf4c0c0f7b3836f95de59ff6abf43907738ae
wallafi <wallafisantos@gmail.com>
2024-07-17 01:24:49 -0300

    add

--- src/modules/code_deploy/outputs.tf
+++ src/modules/code_deploy/outputs.tf
@@ -5,7 +5,7 @@ output "codedeploy_app_name" {
 
 output "codedeploy_deployment_group_name" {
   description = "The name of the CodeDeploy deployment group"
-  value       = aws_codedeploy_deployment_group.group_app_challenge
+  value       = aws_codedeploy_deployment_group.group_app_challenge.name
 }
 
 output "codedeploy_role_arn" {
